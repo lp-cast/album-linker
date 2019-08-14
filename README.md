1cf41962a9bacd6a17aaea9b23a8c303d709ef8b
miripiruni <miripiruni@gmail.com>
2019-08-15 00:07:30 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,48 @@
+# Album Linker for LP Podcast
+
+## Requirements
+
+ - Node.js v8.6.0
+ - npm v6.2.0
+
+## Install
+
+```
+$ git clone https://github.com/lp-cast/album-linker.git && cd album-linker && npm i
+
+```
+
+# Usage
+
+For all links:
+```
+./index.js --artist='Foo Fighters' --album='Wasting Light'
+```
+
+
+
+For one service only:
+```
+./index.js --artist='Foo Fighters' --album='Wasting Light' --service='tidal.com'
+```
+
+Valid services:
+ - music.apple.com
+ - music.yandex.ru
+ - spotify.com
+ - tidal.com
+ 
+Default: `album-linker` search in all services.
+
+For specific output format:
+```
+./index.js --artist='Foo Fighters' --album='Wasting Light' --format='html'
+```
+
+Valid formats:
+ - markdown
+ - html
+ - plaintext
+ - json
+ 
+ Default: `album-linker` output in markdown.
---
+++ src/report.js
@@ -0,0 +1,43 @@
+// {
+//     artist: ''
+//         album: ''
+//     links: [
+//          {
+//              service: 'tidal.com',
+//              link: '…',
+//              text: 'на Apple Music'
+//          },
+//          …
+//     ]
+// }
+
+module.exports = {
+    markdown: (opts) => {
+        return [
+            `Альбом «${opts.album}»`,
+            opts.links.map(item => {
+                return `[${item.text}](${item.link})`;
+            }).join(', ')
+        ].join(' ');
+    },
+
+    plaintext: (opts) => {
+        return [
+            `Альбом «${opts.album}»`,
+            opts.links.map(item => {
+                return `${item.text} ${item.link}`;
+            }).join(', ')
+        ].join(' ');
+    },
+
+    html: (opts) => {
+        return [
+            `Альбом «${opts.album}»`,
+            opts.links.map(item => {
+                return `<a href="${item.link}">${item.text}</a>`;
+            }).join(', ')
+        ].join(' ');
+    },
+
+    json: (opts) => JSON.stringify(opts)
+};
